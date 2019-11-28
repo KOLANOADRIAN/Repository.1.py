937c6e3022d39245583332ed1e3e3b3b55141d2f
Adrian <kolanoadrian@hotmail.com>
2019-11-28 21:20:33 +0100

    Step2.py Game with a year

--- Step2.py
+++
@@ -1,6 +0,0 @@
-#your_age = input('Your age: ')
-#print('Your year of birth: ',2019 - int( your_age))
-
-birth_year = input('Your year of birth: ')
-year = 2019
-print ('You have ',year - (int(birth_year)), 'years old.')
